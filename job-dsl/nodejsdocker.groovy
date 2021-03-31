e762f7f6bd99ead23fd872850f60a8b4916e39e6
Chary Vaangeepuram <srivathsalachary@gmail.com>
2021-03-31 00:41:17 -0700

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,4 +1,4 @@
-job('NodeJS Docker example') {
+job('NodeJS-Docker-example') {
     scm {
         git('git://github.com/svangeepuram/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
